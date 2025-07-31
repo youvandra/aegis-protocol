c5aea78c43ace3ef69af47ddc89d5115f2c44f7e
Youvandra Febrial <40780823+youvandra@users.noreply.github.com>
2025-08-01 01:32:52 +0700

    Updated LegacyPage.tsx

--- src/pages/LegacyPage.tsx
+++ src/pages/LegacyPage.tsx
@@ -37,8 +37,8 @@ const LegacyPage: React.FC = () => {
   return (
     <div className="min-h-screen relative flex flex-col bg-[#D9D9D9]">
       <AestheticNavbar 
-        leftLinkPath="/data"
-        leftLinkText="Data"
+        leftLinkPath="/schedule"
+        leftLinkText="Schedule"
         roomName="Legacy"
         rightLinkPath="/relay"
         rightLinkText="Relay"
