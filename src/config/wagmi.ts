911ba9a90baf4a5dcb3ebd126ba658f7f93d0e29
Youvandra Febrial <40780823+youvandra@users.noreply.github.com>
2025-07-31 23:32:45 +0700

    Updated wagmi.ts

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,8 +1,31 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { mainnet, sepolia, hedera } from 'wagmi/chains'
-import { hederaTestnet } from '../chain'
+import { Chain } from 'wagmi'
+import { hederaTestnet } from '.'
 // Get projectId from https://cloud.walletconnect.com
 export const projectId = 'dd6934ed03aeebf4d1a21fb001d7c1f3' // Replace with your actual project ID
+
+export const hederaTestnet: Chain = {
+  id: 296, // Chain ID for Hedera Testnet
+  name: 'Hedera Testnet',
+  network: 'hedera-testnet',
+  nativeCurrency: {
+    name: 'HBAR',
+    symbol: 'HBAR',
+    decimals: 8,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://testnet.hashio.io/api'],
+    },
+    public: {
+      http: ['https://testnet.hashio.io/api'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'HashScan', url: 'https://hashscan.io/testnet' },
+  },
+}
 
 const metadata = {
   name: 'Aegis Protocol',
@@ -11,10 +34,9 @@ const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-const chains = [mainnet, sepolia, hedera, hederaTestnet] as const
 
 export const config = defaultWagmiConfig({
-  chains,
+  hederaTestnet,
   projectId,
   metadata,
 })
