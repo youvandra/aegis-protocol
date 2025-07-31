4850ce2b56a6b486d53bb7a5ead0f44367cdeefa
Youvandra Febrial <40780823+youvandra@users.noreply.github.com>
2025-07-31 23:30:58 +0700

    Updated wagmi.ts

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,6 +1,6 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { mainnet, sepolia, hedera } from 'wagmi/chains'
-import { hederaTestnet } from './chain'
+import { hederaTestnet } from '../chain'
 // Get projectId from https://cloud.walletconnect.com
 export const projectId = 'dd6934ed03aeebf4d1a21fb001d7c1f3' // Replace with your actual project ID
 
